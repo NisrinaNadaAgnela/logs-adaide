19cb440030a42a898f6171d950d3560f090cb31e
vadlanramdani2203 <vadlanramdani123@gmail.com>
2020-10-19 16:04:32 +0700

    menambahkan Chart

--- resources/views/Logs/logs.blade.php
+++ resources/views/Logs/logs.blade.php
@@ -4,6 +4,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('chartjs/Chart.bundle.js') }}"></script>
+        <style type="text/css">
+            .container {
+                width: 50%;
+                margin: 15px auto;
+            }
+            /* #myChart {
+                width: 50%;
+                height: 100px;
+            } */
+        </style>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -19,26 +30,48 @@
             <div class="card" style="margin-top: 20px">
                 <div class="card-header">Data Logs</div>
                     <div class="card-body">
-                        <table class="table table-bordered table-hover">
-                            <thead>
-                                <tr style="background-color: lightblue">
-                                    <th>Id</th>
-                                    <th>Instance</th>
-                                    <th>Identity</th>
-                                    <th>Date</th>
-                                </tr>
-                            </thead>
-                            @foreach ($logs as $row)
-                            <tbody>
-                                <tr>    
-                                    <td>{{ $row->id }}</td>
-                                    <td>{{ $row->instance }}</td>
-                                    <td>{{ $row->identity }}</td>
-                                    <td>{{ $row->date }}</td>
-                                </tr>
-                            </tbody>
-                            @endforeach
-                        </table>
+                        <div class="chart">
+                            <canvas id="chart"></canvas>
+                            <script>
+                                var ctx = document.getElementById("chart");
+                                var myChart = new Chart(ctx, {
+                                    type: 'line',
+                                    data: {
+                                        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                                        datasets: [{
+                                                label: 'Contoh Chart',
+                                                data: [112, 190, 30, 50, 20, 30],
+                                                backgroundColor: [
+                                                    'rgb(255, 255, 0)'
+                                                    // 'rgba(54, 162, 235, 0.2)',
+                                                    // 'rgba(255, 206, 86, 0.2)',
+                                                    // 'rgba(75, 192, 192, 0.2)',
+                                                    // 'rgba(153, 102, 255, 0.2)',
+                                                    // 'rgba(255, 159, 64, 0.2)'
+                                                ],
+                                                borderColor: [
+                                                    'rgb(253, 215, 3)'
+                                                    // 'rgba(54, 162, 235, 1)',
+                                                    // 'rgba(255, 206, 86, 1)',
+                                                    // 'rgba(75, 192, 192, 1)',
+                                                    // 'rgba(153, 102, 255, 1)',
+                                                    // 'rgba(255, 159, 64, 1)'
+                                                ],
+                                                borderWidth: 3
+                                            }]
+                                    },
+                                    options: {
+                                        scales: {
+                                            yAxes: [{
+                                                    ticks: {
+                                                        beginAtZero: true
+                                                    }
+                                                }]
+                                        }
+                                    }
+                                });
+                            </script>
+                        </div>
                     </div>
                     <!-- <button type="button" name="button" class="btn btn-success"> <a href="logs" style="color: white;">Masuk</a> </button> -->
                 </div>
