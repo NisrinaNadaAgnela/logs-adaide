0391864fb6e50375ba61d264c7de2c99657d75b8
hamsterrina <nisrinanada0203@gmail.com>
2020-10-20 16:01:51 +0700

    Commit Kedua

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
 
+        ConsoleTVs\Charts\ChartsServiceProvider::class,
     ],
 
     /*
