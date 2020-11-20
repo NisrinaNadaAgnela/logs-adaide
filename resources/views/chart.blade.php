2c1cff239a869a28f7c026f90a92ec46f295a0c8
hamsterrina <nisrinanada0203@gmail.com>
2020-11-20 15:58:15 +0700

    state selesai

--- resources/views/chart.blade.php
+++ resources/views/chart.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">	
-	<title>Test HighCharts</title>
+	<title>Grafik State</title>
 </head>
 <body>
 	<div id="chart-container"></div>
